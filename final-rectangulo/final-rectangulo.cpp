fc0fbe9e52720c133ea17586405a85bfcf5570a0
maguimarijuan <maguimarijuan@gmail.com>
2019-07-09 19:49:39 -0300

    agrego el final del rectangulo bien

--- final-rectangulo/final-rectangulo.cpp
+++ final-rectangulo/final-rectangulo.cpp
@@ -15,8 +15,8 @@ typedef struct{
 } Rectangulo;
 
 typedef struct{
-  char id_derecha [20];
-  char id_izquierda [20];
+  char id_rec1[20];
+  char id_rec2 [20];
   Rectangulo rect;
 } PreResultado;
 
@@ -44,66 +44,42 @@ int leerRectangulos(Rectangulo* rectangulos){
 }
 
 bool colisionan(Rectangulo rect1, Rectangulo rect2){
-  bool colisionanEnX =  false;
-  bool colisionanEnY = false;
-  if(rect2.xsi <= rect1.xsd && rect2.xsi >= rect1.xsi) colisionanEnX = true;
-  if(rect2.xsd <= rect1.xsd && rect2.xsd >= rect1.xsi) colisionanEnX = true;
-  if(rect2.ysi <= rect1.ysd && rect2.ysi >= rect1.yid) colisionanEnY = true;
-  if(rect2.yid <= rect1.ysd && rect2.yid >= rect1.yid) colisionanEnY = true;
-  return(colisionanEnX && colisionanEnY);
+
+  if(rect1.xii >= rect2.xsd || rect1.yii >= rect2.ysd) return false;
+  if(rect1.xsd <= rect2.xii || rect1.ysd <= rect2.yii) return false;
+  return true;
 }
 
-Rectangulo obtenerRectanguloInterno(Rectangulo rectDerecha, Rectangulo rectIzquierda){
+Rectangulo obtenerRectanguloInterno(Rectangulo rect1, Rectangulo rect2){
   Rectangulo aux;
-  if (rectDerecha.ysi > rectIzquierda.ysi){
-    // el de la derecha está arriba;
-    aux.xii = rectDerecha.xii;
-    aux.yii = rectDerecha.yii;
-    aux.xid = rectIzquierda.xid;
-    aux.yid = rectDerecha.yid;
-    aux.xsi = rectDerecha.xsi;
-    aux.ysi = rectIzquierda.ysd;
-    aux.xsd = rectIzquierda.xsd;
-    aux.ysd = rectIzquierda.xsd;
-  }
-  else{
-    //el de la derecha está abajo
-    aux.xii = rectDerecha.xii;
-    aux.yii = rectIzquierda.yid;
-    aux.xid = rectIzquierda.xid;
-    aux.yid = rectIzquierda.yid;
-    aux.xsi = rectDerecha.xsi;
-    aux.ysi = rectDerecha.ysi;
-    aux.xsd = rectIzquierda.xsd;
-    aux.ysd = rectDerecha.ysd;
-  }
+  aux.xii = max(rect1.xii, rect2.xii);
+  aux.yii = max(rect1.yii, rect2.yii);
+
+  aux.xsd = min(rect1.xsd, rect2.xsd);
+  aux.ysd = min(rect1.ysd, rect2.ysd);
+
+  aux.xid = aux.xsd;
+  aux.yid = aux.yii;
+  aux.xsi = aux.xii;
+  aux.ysi = aux.ysd;
 
   strcpy(aux.id_rec, "aux");
   return aux;
 }
 
 void obtenerRectanguloInterno(PreResultado* preResultados, int cantPresultados, Rectangulo rect1, Rectangulo rect2){
-  Rectangulo aux;
-  //el rectangulo 2 está a la derecha;
-  if (rect2.xii < rect1.xid){
-    aux = obtenerRectanguloInterno(rect2, rect1);
-    strcpy(preResultados[cantPresultados].id_derecha, rect2.id_rec);
-    strcpy(preResultados[cantPresultados].id_izquierda,rect1.id_rec);
-  }
-  // el rectangulo 1 está a la izquierda;
-  else{
-     aux = obtenerRectanguloInterno(rect1, rect2);
-     strcpy(preResultados[cantPresultados].id_derecha, rect1.id_rec);
-     strcpy(preResultados[cantPresultados].id_izquierda, rect2.id_rec);
-   }
-   preResultados[cantPresultados].rect = aux;
-   return;
+  Rectangulo aux = obtenerRectanguloInterno(rect1, rect2);
+  strcpy(preResultados[cantPresultados].id_rec1, rect1.id_rec);
+  strcpy(preResultados[cantPresultados].id_rec2,rect2.id_rec);
+
+  preResultados[cantPresultados].rect = aux;
+  return;
 
 }
 
 bool estaEnPreResultado(Rectangulo rect, PreResultado preResultado){
-  if(strcmp(rect.id_rec, preResultado.id_derecha) == 0) return true;
-  if(strcmp(rect.id_rec, preResultado.id_izquierda) == 0) return true;
+  if(strcmp(rect.id_rec, preResultado.id_rec1) == 0) return true;
+  if(strcmp(rect.id_rec, preResultado.id_rec2) == 0) return true;
   return false;
 }
 
@@ -119,23 +95,19 @@ void cambiarNegativo(float* numero){
 }
 
 float obtenerAreaResultado(Rectangulo rect, PreResultado preResultado){
-  Rectangulo aux;
-  if (preResultado.rect.xii < rect.xid) aux = obtenerRectanguloInterno(preResultado.rect, rect);
-  // el rectangulo 1 está a la izquierda;
-  else aux = obtenerRectanguloInterno(rect, preResultado.rect);
-  float base = aux.xid - aux. xii;
+  Rectangulo aux = obtenerRectanguloInterno(rect,preResultado.rect);
+
+  float base = aux.xid - aux.xii;
   cambiarNegativo(&base);
+
   float altura = aux.ysd - aux.yid;
   cambiarNegativo(&altura);
   return(base * altura);
 }
 
 float obtenerDistancia(Rectangulo rect, PreResultado preResultado){
-  Rectangulo aux;
-  if (preResultado.rect.xii < rect.xid) aux = obtenerRectanguloInterno(preResultado.rect, rect);
-  // el rectangulo 1 está a la izquierda;
-  else aux = obtenerRectanguloInterno(rect, preResultado.rect);
-  float distancia = sqrt(pow(aux.xii,2) + pow(aux.yii,2));
+  Rectangulo  aux = obtenerRectanguloInterno(preResultado.rect, rect);
+  float distancia = sqrt(pow(aux.xsi,2) + pow(aux.ysi,2));
   return distancia;
 }
 
@@ -143,19 +115,19 @@ float obtenerDistancia(Rectangulo rect, PreResultado preResultado){
 int procesarRectangulos(Rectangulo* rectangulos, int cantidadDeRectangulos, Resultado* resultados){
 
   PreResultado preResultados [20];
-  int cantPresultados = 0;
+  int cantPreResultados = 0;
 
   for(int i = 0; i < cantidadDeRectangulos; i++){
-    for(int j = 0; j < cantidadDeRectangulos; j++){
+    for(int j = i+1; j < cantidadDeRectangulos; j++){
       if (i == j) continue;
       if(colisionan(rectangulos[i],rectangulos[j])){
-        obtenerRectanguloInterno(preResultados, cantPresultados, rectangulos[i], rectangulos[j]);
-        cantPresultados++;
+        obtenerRectanguloInterno(preResultados, cantPreResultados, rectangulos[i], rectangulos[j]);
+        cantPreResultados++;
       }
     }
   }
   int cantidadDeResultados = 0;
-  for(int i = 0; i < cantPresultados; i++){
+  for(int i = 0; i < cantPreResultados; i++){
     for(int j = 0;  j < cantidadDeRectangulos; j++){
       if(estaEnPreResultado(rectangulos[j], preResultados[i])) continue;
       if(colisionan(rectangulos[j], preResultados[i])){
@@ -163,8 +135,8 @@ int procesarRectangulos(Rectangulo* rectangulos, int cantidadDeRectangulos, Resu
         float distancia = obtenerDistancia(rectangulos[j], preResultados[i]);
         resultados[cantidadDeResultados].distancia = distancia;
         resultados[cantidadDeResultados].area = area;
-        strcpy(resultados[cantidadDeResultados].id_rec1,preResultados[i].id_derecha);
-        strcpy(resultados[cantidadDeResultados].id_rec2,preResultados[i].id_izquierda);
+        strcpy(resultados[cantidadDeResultados].id_rec1,preResultados[i].id_rec1);
+        strcpy(resultados[cantidadDeResultados].id_rec2,preResultados[i].id_rec2);
         strcpy(resultados[cantidadDeResultados].id_rec3,rectangulos[j].id_rec);
         cantidadDeResultados ++;
       }
@@ -182,15 +154,69 @@ int comparar(const void* r1, const void* r2){
   return -1;
 }
 
+typedef struct{
+  char id_rec1[20];
+  char id_rec2[20];
+  char id_rec3[20];
+  bool repetido;
+} IDS;
+
+bool sonIguales(IDS ids1, IDS ids2){
+  bool id_rec1 = false;
+  bool id_rec2 = false;
+  bool id_rec3 = false;
+  if(strcmp(ids1.id_rec1, ids2.id_rec1) == 0 ||
+    strcmp(ids1.id_rec1, ids2.id_rec2) == 0 ||
+    strcmp(ids1.id_rec1, ids2.id_rec3) == 0 ) id_rec1 = true;
+  if(strcmp(ids1.id_rec2, ids2.id_rec1) == 0 ||
+    strcmp(ids1.id_rec2, ids2.id_rec2) == 0 ||
+    strcmp(ids1.id_rec2, ids2.id_rec3) == 0 ) id_rec2 = true;
+  if(strcmp(ids1.id_rec3, ids2.id_rec1) == 0 ||
+    strcmp(ids1.id_rec3, ids2.id_rec2) == 0 ||
+    strcmp(ids1.id_rec3, ids2.id_rec3) == 0 ) id_rec3 = true;
+  return id_rec1&&id_rec2&&id_rec3;
+
+}
+
+int eliminarRepetidos(Resultado* resultados, int cantidadDeResultados, Resultado* resultadosSinRepetidos){
+  int cantidadDeResultadosSinRepetidos = 0;
+  IDS ids [20];
+  for(int i = 0; i < cantidadDeResultados; i ++){
+    strcpy(ids[i].id_rec1, resultados[i].id_rec1);
+    strcpy(ids[i].id_rec2, resultados[i].id_rec2);
+    strcpy(ids[i].id_rec3, resultados[i].id_rec3);
+    ids[i].repetido = false;
+
+  }
+  for(int i = 0; i < cantidadDeResultados; i++){
+    for(int j = i+1; j < cantidadDeResultados; j++){
+      if(i == j) continue;
+      if(sonIguales(ids[i], ids[j])) ids[j].repetido = true;
+    }
+  }
+
+  for(int i = 0; i <cantidadDeResultados; i++){
+    if(ids[i].repetido) continue;
+    resultadosSinRepetidos[cantidadDeResultadosSinRepetidos].distancia =resultados[i].distancia;
+    resultadosSinRepetidos[cantidadDeResultadosSinRepetidos].area = resultados[i].area;
+    strcpy(resultadosSinRepetidos[cantidadDeResultadosSinRepetidos].id_rec1, resultados[i].id_rec1);
+    strcpy(resultadosSinRepetidos[cantidadDeResultadosSinRepetidos].id_rec2, resultados[i].id_rec2);
+    strcpy(resultadosSinRepetidos[cantidadDeResultadosSinRepetidos].id_rec3, resultados[i].id_rec3);
+    cantidadDeResultadosSinRepetidos++;
+  }
+  return cantidadDeResultadosSinRepetidos;
+}
 
 int main(){
   Rectangulo rectangulos[20];
   int cantidadDeRectangulos = leerRectangulos(rectangulos);
   Resultado resultados[20];
   int cantidadDeResultados = procesarRectangulos(rectangulos,cantidadDeRectangulos,resultados);
-  qsort(resultados, cantidadDeResultados, sizeof(Resultado), comparar);
+  Resultado resultadosSinRepetidos [20];
+  cantidadDeResultados =  eliminarRepetidos(resultados, cantidadDeResultados,resultadosSinRepetidos);
+  qsort(resultadosSinRepetidos, cantidadDeResultados, sizeof(Resultado), comparar);
   cout<<"DISTANCIA"<<"\t\t"<<"AREA"<<"\t\t"<<"ID REC 1"<<"\t\t"<<"ID REC 2"<<"\t\t"<<"ID REC 3"<< endl;
   for(int i = 0; i < cantidadDeResultados; i++){
-    cout<<resultados[i].distancia<<"\t\t"<< resultados[i].area<<"\t\t"<< resultados[i].id_rec1<<"\t\t"<< resultados[i].id_rec2<<"\t\t"<< resultados[i].id_rec3<<endl;
+    cout<<resultadosSinRepetidos[i].distancia<<"\t\t"<< resultadosSinRepetidos[i].area<<"\t\t"<< resultadosSinRepetidos[i].id_rec1<<"\t\t"<< resultadosSinRepetidos[i].id_rec2<<"\t\t"<< resultadosSinRepetidos[i].id_rec3<<endl;
   }
 }
